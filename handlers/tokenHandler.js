633936156218eba520565ea99d71281613222a3f
Dennis Joel <dennismwagiru@gmail.com>
2021-04-02 12:52:46 +0300

    Finalize on token handler.

--- handlers/tokenHandler.js
+++ handlers/tokenHandler.js
@@ -69,7 +69,14 @@ handler.post = (request) => {
 // Optional fields: none
 handler.get = request => {
     return new Promise((resolve, reject) => {
-        resolve(200);
+        const rules = {id: ['required']}
+        validationHelper.validate(rules, request.queryString).then(() => {
+            _data.read('tokens', request.queryString.id).then(tokenData => {
+                resolve(tokenData);
+            }, err => {
+                reject({ statusCde: 404 });
+            })
+        }, err => reject(err));
     });
 };
 
@@ -78,7 +85,25 @@ handler.get = request => {
 // Optional fields: none
 handler.put = request => {
     return new Promise((resolve, reject) => {
-        resolve(200);
+        const rules = {id: ['required'], extend: ['required', 'boolean']}
+        validationHelper.validate(rules, request.payload).then(() => {
+            if (request.payload.extend === true) {
+                _data.read('tokens', request.payload.id).then(tokenData => {
+                    if (tokenData.expires > Date.now()) {
+                        tokenData.expires = Date.now() + 1000 * 60 * 60;
+
+                        // Store the new updates
+                        _data.update('tokens', request.payload.id, tokenData).then(() => {
+                            resolve();
+                        }, err => reject({ statusCode: 400, payload: { 'Error': 'Could not update the token\'s expiration.' } }));
+                    } else {
+                        reject({ statusCode: 400, payload: { 'Error': 'The token has already expired and cannot be extended' } });
+                    }
+                }, err => reject({ statusCode: 404, payload: { 'Error': 'Specified token does not exist' } }));
+            } else {
+                reject({ statusCode: 400, payload: { 'Error': 'Extend should be true' } });
+            }
+        }, err => reject(err));
     });
 };
 
@@ -87,7 +112,14 @@ handler.put = request => {
 // Optional fields: none
 handler.delete = request => {
     return new Promise((resolve, reject) => {
-        resolve(200);
+        const rules = {id: ['required']};
+        validationHelper.validate(rules, request.queryString).then(() => {
+            _data.read('tokens', request.queryString.id).then(tokenData => {
+                _data.delete('tokens', request.queryString.id).then(() => {
+                    resolve();
+                }, err => reject({ statusCode: 500, payload: { 'Error': 'Could not delete the specified token.' } }));
+            }, err => reject({ statusCode: 404, payload: { 'Error': 'Could not find the specified token.' } }));
+        }, err => reject(err));
     });
 };
 
